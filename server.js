426322723e97005f2d95827426cdb250f0dbae03
unknown <abolfazl.amani@PART.LOCAL>
2025-03-17 14:48:56 +0330

    Finish the project about managing library

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const fs = require('fs')
 const url = require('url')
 const { NotFoundError, DonNotWritingFile} = require('./AppError')
 const db = require('./db.json')
-// console.log(db.users)
 let primaryCrime = 0
+function validUser(database, nUser){
+    return database.users.some((user) => (user.username === nUser.username || user.email === nUser.email))
+}
 const server = http.createServer((req, res) => {
-    if(req.method === "GET" && req.url === "/api/users"){
+    if(req.method === "GET" && req.url === "/api/users"){ // GetAllUsers API
         fs.readFile("db.json", (err, data) => {
             if(err){
                 res.writeHead(500, {'Content-type': 'text/plain'})
@@ -23,7 +25,7 @@ const server = http.createServer((req, res) => {
                 console.log("Parse Error")
             }
         })
-    }else if(req.method === "GET" && req.url === "/api/books"){
+    }else if(req.method === "GET" && req.url === "/api/books"){ // GetAllBooks API
         fs.readFile("db.json", (err, data) => {
             if(err){
                 res.writeHead(500, {'Content-type': 'text/plain'})
@@ -40,7 +42,58 @@ const server = http.createServer((req, res) => {
                 console.log("Parse Error")
             }
         })
-    }else if(req.method === "DELETE" && req.url.startsWith("/api/books")){ 
+    }else if(req.method === "POST" && req.url === "/api/books/rents"){ // ReserveBook API
+        console.log("Welcome to RESERVE")
+        let _ = ""
+        req.on('data', (chunk) => _ += chunk.toString())
+        req.on('end', () => {
+            const {userId, title} = JSON.parse(_)
+            const indexBook = db.books.findIndex((book) => book.title === title)
+            console.log(title)
+            if(indexBook !== -1){
+                if(db.books[indexBook].free === 1){
+                    const newRent = {id: Math.random(), userId: userId, bookId: db.books[indexBook].id}
+                    db.rents.push(newRent)
+                    db.books[indexBook].free = 0
+                    fs.writeFile("db.json", JSON.stringify(db), (err) => {
+                        if(err){
+                            throw new DonNotWritingFile(); 
+                        }
+                        res.writeHead(201, {"Content-type": 'application/json'})
+                        res.write(JSON.stringify({massage: "Reserving Book is Successful"}))
+                        res.end()
+                    })
+                }else{
+                    res.writeHead(401, {'Content-type': 'application/json'})  
+                    res.write(JSON.stringify({massage: "Book Were Reserved!!!"}))    
+                    res.end()
+                }
+            }else{
+                  res.writeHead(409, {'Content-type': 'application/json'})  
+                  res.write(JSON.stringify({massage: "Book Not Found!!!"}))    
+                  res.end()                                                  
+            }
+        })
+    }else if(req.method === "PUT" && req.url === "/api/books/back"){
+        console.log("welcome to Back")
+        let _ = ""
+        req.on('data', (chunk) => _ += chunk.toString())
+        req.on('end', () => {
+            const backBook = JSON.parse(_)
+            const indexBook = db.books.findIndex((book) => book.title === backBook.title)
+            db.books[indexBook].free = 1
+            const reminedRents = db.rents.filter((rent) => rent.bookId !== db.books[indexBook].id)
+            console.log(reminedRents)
+            fs.writeFile("db.json", JSON.stringify({...db, rents: reminedRents}), (err) => {
+                if(err){
+                    throw new DonNotWritingFile();
+                }
+                res.writeHead(202, {"Content-type": 'application/json'})
+                res.write(JSON.stringify({massage: "Freeing Book is Successful"}))
+                res.end()
+            })
+        })
+    }else if(req.method === "DELETE" && req.url.startsWith("/api/books")){ // DeleteBook API
         const urlParams = url.parse(req.url, true)
         console.log(urlParams)
         const bookId = parseInt(urlParams.query.id)
@@ -77,7 +130,7 @@ const server = http.createServer((req, res) => {
         }
         // res.end("Test Response")
         
-    }else if(req.method === "POST" && req.url ==="/api/books"){
+    }else if(req.method === "POST" && req.url ==="/api/books"){ // AddBook API
         let book = ""
         req.on('data', (chunk) => {
             book += chunk.toString()
@@ -96,7 +149,8 @@ const server = http.createServer((req, res) => {
             res.end("Adding New Book Success.")
         })
         
-    }else if(req.method === "PUT" && req.url.startsWith("/api/books")){
+    }else if(req.method === "PUT" && req.url.startsWith("/api/books")){ // UpdataBook API
+        console.log("Welcome to 'Book")
         const urlParams = url.parse(req.url, true)
         const id = parseInt(urlParams.query.id)
         if(db.books.some(book => book.id === id)){
@@ -123,7 +177,7 @@ const server = http.createServer((req, res) => {
                         throw new DonNotWritingFile();
                     }
                     res.writeHead(202, {"Content-type": 'application/json'})
-                    res.write(JSON.stringify({massage: "Updating File Success"}))
+                    res.write(JSON.stringify({massage: "Updating Book Success"}))
                     res.end()
                 })
             })
@@ -132,34 +186,76 @@ const server = http.createServer((req, res) => {
             res.write(JSON.stringify({massage: "Book Not Found!!!"}))
             res.end()
         }
-    }else if(req.method === "POST" && req.url === "/api/users"){
-        let user = ""
+    }else if(req.method === "POST" && req.url === "/api/users/login"){ // Login API
+        let _ = ""
+        req.on('data', (chunk) => _ += chunk.toString())
+        req.on('end', () => {
+            const {username, email} = JSON.parse(_)
+            if(db.users.some((user) => user.username === username && user.email === email)){
+                res.writeHead(200, {'Content-type': 'application/json'})
+                res.write(JSON.stringify({username: username, email: email}))
+                res.end()
+            }else{
+                res.writeHead(401, {'Content-type': 'application/json'})
+                res.write(JSON.stringify({massage: "You Need to Register"}))
+                res.end()
+            }
+        })
+    }else if(req.method === "POST" && req.url === "/api/users"){ // Add or RegisterUser API
+        let user = "";
         req.on('data', (chunk) => {
             user += chunk.toString()
         })
         req.on('end', () => {
-            const newUser = {id: Math.random(), ...JSON.parse(user), crime: 0, role: "USER"}
-            db.users.push(newUser)
+            if(validUser(db, JSON.parse(user))){
+                res.writeHead(422, {'Content-type': 'application/json'})
+                res.write(JSON.stringify({massage: "Eamil or Username already is Exist!!!"}))
+                res.end()
+            }else if(JSON.parse(user).name === "" || JSON.parse(user).username === "" || JSON.parse(user).email === ""){
+                res.writeHead(422, {'Content-type': 'application/json'})
+                res.write(JSON.stringify({massage: "Your Data is not Valid!!!"}))
+                res.end()
+            }else{
+                const newUser = {id: Math.random(), ...JSON.parse(user), crime: 0, role: "USER"}
+                db.users.push(newUser)
+                fs.writeFile("db.json", JSON.stringify(db), (err) => {
+                    if(err){
+                        throw new DonNotWritingFile();
+                    }
+                    res.writeHead(201, {"Content-type": 'application/json'})
+                    res.write(JSON.stringify({massage: "Updating File Success"}))
+                    res.end()
+                })
+            }
+        })
+    }else if(req.method === "PUT" && req.url.startsWith("/api/users/upgrade")){  // UpgradingUser API
+        console.log("Welcome to Upgrade")
+        const urlParams = url.parse(req.url, true)
+        const userId = parseInt(urlParams.query.id)
+        if(db.users.some((user) => user.id === userId)){
+            const index = db.users.findIndex((user) => user.id === userId)
+            db.users[index].role = "ADMIN"
             fs.writeFile("db.json", JSON.stringify(db), (err) => {
                 if(err){
-                    throw new DonNotWritingFile();
+                    throw new DonNotWritingFile(); 
                 }
-                res.writeHead(201, {"Content-type": 'application/json'})
-                res.write(JSON.stringify({massage: "Updating File Success"}))
+                res.writeHead(200, {"Content-type": 'application/json'})
+                res.write(JSON.stringify({massage: "Upgrading USER to ADMIN are Successfully"}))
                 res.end()
             })
-        })
-    }else if(req.method === "PUT" && req.url.startsWith("/api/users")){
+        }
+    }else if(req.method === "PUT" && req.url.startsWith("/api/users")){ // UpdataCrime API
+
         const urlParams = url.parse(req.url, true)
         const id = parseInt(urlParams.query.id)
         if(db.users.some((user) => user.id === id)){
             // primaryCrime++
             // db.users[index].crime = primaryCrime
-            const reqBody = ""
+            let reqBody = ""
             req.on('data', (chunk) => reqBody += chunk.toString())
             req.on('end', () => {
                 const index = db.users.findIndex((user) => user.id === id)
-                db.users[index].crime = reqBody.crime
+                db.users[index].crime = JSON.parse(reqBody).crime
                 fs.writeFile("db.json", JSON.stringify(db), (err) => {
                     if(err){
                         res.writeHead(500, {'Content-type': 'text/plain'})
@@ -173,12 +269,11 @@ const server = http.createServer((req, res) => {
             })
         }else{
             res.writeHead(401, {'Content-type': 'application/json'})
-            res.write(JSON.stringify({massage: "Book Not Found!!!"}))
+            res.write(JSON.stringify({massage: "ID Not Found!!!"}))
             res.end()
         }
     }
 })
-
 server.listen(4000, () => {
     console.log("Server is running on port 4000")
 })
